aa1c064e95ad0893990a282e51adf89c0d18fba8
unknown <appstoregugle2310@gmail.com>
2015-06-13 00:36:46 +0700

    phongnhung - add java code

---
+++ PCShutdownTimer/src/pcshutdowntimer/PCShutdownTimer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pcshutdowntimer;
+
+/**
+ * @author PhongVu
+ */
+public class PCShutdownTimer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PCShutdownTimerFrame a = new PCShutdownTimerFrame();
+        a.show();
+    }
+    
+}
